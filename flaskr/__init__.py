d31d95380f13afb689a493577dea941512beffe0
Melanie Davila <davilam91@gmail.com>
2017-08-04 12:54:24 -0400

    Complete tutorial

---
+++ flaskr/__init__.py
@@ -0,0 +1,3 @@
+# simplifies locating the application
+# brings the application instance into the top-level of the application package
+from .flaskr import app
--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -8,10 +8,101 @@ app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , flaskr.py
 
 # Load default config and override config from an environment variable
+# Config object works similarly to a dictionary so it can be updated with new
+# values.
 app.config.update(dict(
+	# app.root_path can be used to get the path to the application
     DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
     PASSWORD='default'
 ))
+# load a separate, environment-specific config file. Flask allows you to import
+# multiple configurations
+# FLASKR_SETTINGS points to a config file to be loaded.
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+# Allows for easy connections to the specified database.
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+# Functions marked with teardown_appcontext() are called every time the app context 
+# tears down. What does this mean? Essentially, the app context is created before 
+# the request comes in and is destroyed (torn down) whenever the request finishes. 
+# A teardown can happen because of two reasons: either everything went well (the 
+# error parameter will be None) or an exception happened, in which case the 
+# error is passed to the teardown function.
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+
+# View Functions
+@app.route('/')
+# This will pass entries to the show_entries.html template and return the 
+# rendered one
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
